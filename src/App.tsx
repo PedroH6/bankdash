67e12cd3656d7b00dffb6b01bdc0ccc13796e978
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-04-18 12:05:47 -0300

    Finalisando o header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { HomeIcon } from "lucide-react"
 import { Sidebar, SidebarItem } from "./components/Sidebar"
+import { Header } from "./components/Header"
 
 // Exemplo de como usar o componente Sidebar 
 const sidebarItems: SidebarItem[] = [
@@ -18,7 +19,12 @@ const sidebarItems: SidebarItem[] = [
 function App() {
   return (
     <>
+    <div className="flex h-screen">
       <Sidebar items={sidebarItems}/>
+      <div className="flex-1 flex flex-col">
+        <Header />
+      </div>
+    </div>
     </>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,31 @@
+import { BellIcon, SearchIcon, SettingsIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Avatar, AvatarImage } from "./ui/avatar";
+
 export function Header() {
     return(
-        <div className="bg-white shadow-md px-6 flex items-center justify-between">
+        <div className="container bg-white shadow-md px-5 py-3 flex items-center justify-between">
             <div className="text-xl font-semibold">Overview</div>
+
             <div className="flex items-center space-x-4">
-                
+
+                <div className="relative flex items-center mt-2">
+                    <span className="absolute">
+                        <SearchIcon className="w-6 h-6 mx-3 text-gray-400 dark:text-gray-500"/>
+                    </span>
+                    <Input type="text" placeholder="Search for something" className="block w-full py-2.5 text-gray-700 placeholder-gray-400/70 bg-white border border-gray-200 rounded-xl pl-11 pr-5 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                </div>
+
+                <Button variant="ghost" size="icon" >
+                    <SettingsIcon className="h-5 w-5"/>
+                </Button>
+                <Button variant="ghost" size="icon">
+                    <BellIcon className="h-5 w-5"/>
+                </Button>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="Avatar"/>
+                </Avatar>
             </div>
         </div>
     )
